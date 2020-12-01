51fb7e9cf9e3ca4d728743ebb1ece33ab2c7c5fd
Tawishi <tawishisharma1@gmal.com>
2020-12-01 15:56:11 +0530

    Created tree with 4 nodes

--- Trees/Traversal.cpp
+++ Trees/Traversal.cpp
@@ -1,8 +1,20 @@
 #include<iostream>
 using namespace std;
-class node  
-{ public :
+class node  { 
+public :
   int data; 
   struct node *left; 
-  struct node *right; 
+  struct node *right;
+   node(int value) {
+       data = value;
+       left = NULL;
+       right = NULL;
+   }
 };
+
+int main() {
+    node * root = new node(1);
+    root->left = new node(2);
+    root->right = new node(3);
+    root->left->left = new node(4);
+}
