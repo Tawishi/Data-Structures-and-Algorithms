28836c5c9b50987e5a5854d680e107fa15074ebc
Tawishi <tawishisharma1@gmail.com>
2020-12-03 21:57:58 +0530

    Added driver function to insertion.cpp

--- Trees/BST/insertion.cpp
+++ Trees/BST/insertion.cpp
@@ -2,22 +2,29 @@
 
 using namespace std;
 
-class node {
+class BST {
 public:
     int data;
-    node * left;
-    node * right;
+    BST * left;
+    BST * right;
 
-    node(int val) {
+    BST();
+    BST(int);
+    BST* insert(BST * ,int);
+    void inorder(BST*);
+};
+
+BST ::BST() : data(0), left(NULL), right(NULL) {}
+
+BST ::BST(int val) {
         data = val;
         left = right = NULL;
     }
-};
 
-node * insert (node * root, int key) {
-    node * n = new node(key);
+BST* BST:: insert (BST* root, int key ) {
+    BST * n = new BST(key);
 
-    if(root==NULL)
+    if(!root)
         return n;
     
     if(key > root->data)
@@ -27,9 +34,26 @@ node * insert (node * root, int key) {
         root->left = insert(root->left, key);
     
     return root;
+}
 
+void BST::inorder(BST* root) {
+    if(!root) 
+        return;
+    inorder(root->left);
+    cout<<root->data<<" ";
+    inorder(root->right);
 }
 
 int main() {
-
+    BST b, *root = NULL; 
+    root = b.insert(root, 50); 
+    b.insert(root, 30); 
+    b.insert(root, 20); 
+    b.insert(root, 40); 
+    b.insert(root, 70); 
+    b.insert(root, 60); 
+    b.insert(root, 80); 
+  
+    b.inorder(root); 
+    return 0; 
 }
