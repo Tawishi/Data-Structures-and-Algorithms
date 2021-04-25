f1c9c2a844bb8fba5c4bd85a51e63f95a014e983
Tawishi <55306738+Tawishi@users.noreply.github.com>
2021-04-25 21:40:39 +0530

    Resolving compile time errors

--- OS_practical/Scheduling Algorithms/FCFS.cpp
+++ OS_practical/Scheduling Algorithms/FCFS.cpp
@@ -1,16 +1,9 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void findAverageTime(vector<int> burst_time) {
-	float average_waiting_time =  findTotalTime(findWaitingTime(burst_time))/burst_time.size();
-	cout << "Processes  "<< " Burst time  "
-         << " Waiting time  \n";
-	cout<<"Average waiting time = "<<average_waiting_time<<"\n";
-}
-
 vector<int> findWaitingTime(vector<int> burst_time) {
 	int size = burst_time.size();
-	int waiting_time[size];
+	vctor<int> waiting_time;
 	waiting_time[0] = 0;
 	for(int i=1;i<size;i++) {
 		waiting_time[i] = waiting_time[i-1]+burst_time[i-1];
@@ -25,11 +18,18 @@ int findTotalTime(vector<int> values) {
 	return sum;
 }
 
+void findAverageTime(vector<int> burst_time) {
+	float average_waiting_time =  findTotalTime(findWaitingTime(burst_time))/burst_time.size();
+	cout << "Processes  "<< " Burst time  "
+         << " Waiting time  \n";
+	cout<<"Average waiting time = "<<average_waiting_time<<"\n";
+}
+
 int main() {
 	vector<int> burst_time;
 	int i=0;
 	char choice='y';
-	while(choice='y' || choice='Y') {
+	while(choice=='y' || choice=='Y') {
 		cout<<"Enter process CPU burst time (milliseconds): ";
 		cin>>burst_time[i];
 		i++;
