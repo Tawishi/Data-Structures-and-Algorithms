b0c57d4bfea745b5cbc8b1bf27c441fceab2abe4
Tawishi <55306738+Tawishi@users.noreply.github.com>
2021-04-25 21:57:57 +0530

    Resolve runtime error

--- OS_practical/Scheduling Algorithms/FCFS.cpp
+++ OS_practical/Scheduling Algorithms/FCFS.cpp
@@ -6,7 +6,7 @@ vector<int> findWaitingTime(vector<int> burst_time) {
 	vector<int> waiting_time;
 	waiting_time[0] = 0;
 	for(int i=1;i<size;i++) {
-		waiting_time[i] = waiting_time[i-1]+burst_time[i-1];
+		waiting_time[i] = waiting_time[i-1] + burst_time[i-1];
 	}
 	return waiting_time;
 }
@@ -15,11 +15,13 @@ int findTotalTime(vector<int> values) {
 	int sum=0;
 	for(int i=0;i<values.size();i++)
 		sum += values[i];
+	cout<<"\nSum "<<sum<<"\n";
 	return sum;
 }
 
 void findAverageTime(vector<int> burst_time) {
-	float average_waiting_time =  findTotalTime(findWaitingTime(burst_time))/burst_time.size();
+	int totalWaitingTime = findTotalTime(findWaitingTime(burst_time));
+	float average_waiting_time =  totalWaitingTime/burst_time.size();
 	cout << "Processes  "<< " Burst time  "
          << " Waiting time  \n";
 	cout<<"Average waiting time = "<<average_waiting_time<<"\n";
