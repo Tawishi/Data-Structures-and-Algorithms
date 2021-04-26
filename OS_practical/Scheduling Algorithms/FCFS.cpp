18a054af9c00d11cf4a7ab38d708189b13bd4781
Tawishi <tawishisharma1@gmail.com>
2021-04-26 15:42:45 +0530

    Added arrival time to FCFS scheduling

--- OS_practical/Scheduling Algorithms/FCFS.cpp
+++ OS_practical/Scheduling Algorithms/FCFS.cpp
@@ -1,57 +1,62 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<int> findWaitingTime(vector<int> burst_time) {
-	vector<int> waiting_time (burst_time.size(),0);
+vector<float> findWaitingTime(vector<float> arrival_time, vector<float> burst_time) {
+	vector<float> waiting_time (burst_time.size(),0);
 	for(int i=1;i<waiting_time.size();i++) {
-		waiting_time[i] = waiting_time[i-1] + burst_time[i-1];
+		waiting_time[i] = waiting_time[i-1] + burst_time[i-1] - arrival_time[i];
 	}
 	return waiting_time;
 }
 
-vector<int> findTurnAroundTime(vector<int> burst_time, vector<int> waiting_time) {
-	vector<int> turn_around_time (burst_time.size(),0);
+vector<float> findTurnAroundTime(vector<float> burst_time, vector<float> waiting_time) {
+	vector<float> turn_around_time (burst_time.size(),0);
 	for(int i=0;i<turn_around_time.size();i++) {
 		turn_around_time[i] = burst_time[i] + waiting_time[i];
 	}
 	return turn_around_time;
 }
 
-int findTotalTime(vector<int> values) {
-	int sum=0;
+float findTotalTime(vector<float> values) {
+	float sum=0;
 	for(int i=0;i<values.size();i++)
 		sum += values[i];
 	return sum;
 }
 
-void findAverageTime(vector<int> burst_time) {
-        vector<int> waiting_time_values = findWaitingTime(burst_time);
-	int totalWaitingTime = findTotalTime(waiting_time_values);
-	float average_waiting_time = float(totalWaitingTime)/float(waiting_time_values.size());
+void findAverageTime(vector<float> arrival_time, vector<float> burst_time) {
+        vector<float> waiting_time_values = findWaitingTime(arrival_time, burst_time);
+	float totalWaitingTime = findTotalTime(waiting_time_values);
+	float average_waiting_time = totalWaitingTime/waiting_time_values.size();
 
-	vector<int> turn_around_time_values = findTurnAroundTime(burst_time,waiting_time_values);
-	int totalTurnAroundTime = findTotalTime(turn_around_time_values);
-	float average_turn_around_time = float(totalTurnAroundTime)/float(turn_around_time_values.size());
+	vector<float> turn_around_time_values = findTurnAroundTime(burst_time,waiting_time_values);
+	float totalTurnAroundTime = findTotalTime(turn_around_time_values);
+	float average_turn_around_time = totalTurnAroundTime/turn_around_time_values.size();
 
-	cout << "Processes  "<< " Burst time  "
-         << " Waiting time\t\t"<<"Turn Around Time \n";
+	cout << "Processes  "<<"Arrival time " <<" Burst time  "
+         << "\tWaiting time\t"<<"Turn Around Time \n";
 	for(int i=0;i<burst_time.size();i++) {
-		cout<<i+1<<"\t\t"<<burst_time[i]<<"\t\t"<<waiting_time_values[i]<<"\t\t"<<turn_around_time_values[i]<<"\n";
+		cout<<i+1<<"\t\t"<<arrival_time[i]<<"\t\t"<<burst_time[i]<<"\t\t"<<waiting_time_values[i]<<"\t\t"<<turn_around_time_values[i]<<"\n";
 	}
 	cout<<"Average waiting time = "<<average_waiting_time<<"\n";
 	cout<<"Average turn-around time = "<<average_turn_around_time<<"\n";
 }
 
 int main() {
-	vector<int> burst_time;
-	int i=0, time, n;
+	vector<float> burst_time, arrival_time;
+	int n,i=1;
+	float time, arrival_time_input;
         cout<<"Enter the number of processes : ";
         cin>>n;
 	while(n--) {
+		cout<<"Process "<<i++<<"\n";
+		cout<<"Enter arrival time: ";
+		cin>>arrival_time_input;
+		arrival_time.push_back(arrival_time_input);
 		cout<<"Enter process CPU burst time (milliseconds): ";
 		cin>>time;
 		burst_time.push_back(time);
 	}
-        findAverageTime(burst_time);
+        findAverageTime(arrival_time,burst_time);
 	return 0;
 }
