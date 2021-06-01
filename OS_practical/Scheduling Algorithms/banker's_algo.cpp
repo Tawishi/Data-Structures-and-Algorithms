defa6f11bf698595cd03c8e0b3f0473689e0ef9d
Tawishi <tawishisharma1@gmail.com>
2021-06-01 12:15:25 +0530

    File added

--- OS_practical/Scheduling Algorithms/banker's_algo.cpp
+++ OS_practical/Scheduling Algorithms/banker's_algo.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-const size_t m = 3,n = 5;
+const int m = 3,n = 5;
 
 
 // resouce allocation
@@ -10,11 +10,14 @@ void resource(int need[n][m], int available[m], int allocation[n][m]) {
     int request[n][m];
     for(int i=0;i<n;i++) {
         for(int j=0;j<m;j++) {
-            if(request[i][j] > need[i][j])
+            if(request[i][j] > need[i][j]) {
                 cout<<"error\n"; //error
-            if(request[i][j] > available[j])
+                return;
+            }
+            if(request[i][j] > available[j]) {
                 cout<<"wait\n";
-            
+                return;
+            }
             available[j] -= request[i][j];
             allocation[i][j] += request[i][j];
             need[i][j] -= request[i][j]; 
@@ -33,32 +36,44 @@ void calculateNeed(int need[n][m],int max_resources[n][m], int allocation[n][m])
 }
 
 // safety algorithm
-vector<int> safety(int max_resources[n][m], int available[m], int need[n][m], int allocation[n][m]) {
+void safety(int max_resources[n][m], int available[m], int allocation[n][m]) {
+    cout<<"Entered the function";
+    int need[n][m];
+    calculateNeed(need, max_resources, allocation);
     int work[m];
-    vector<int> safeSeq;
+    int safeSeq[n];
     for(int i=0;i<m;i++) {
         work[i] = available[i];
     }
+
     bool finish[n]; // false for all n
-    for(int i=0;i<m;i++) {
+    for(int i=0;i<n;i++) {
         finish[i] = false;
     }
-    for(int i=0;i<n;i++) {
-        if(finish[i]==false) {
-            
-            for(int j=0;j<m;j++) {
-                if(need[i][j] > work[j])
-                    break;
-                // need[i] <= work
-                work[j] += allocation[i][j];
-                cout<<work[j]<<" ";
-                finish[i] = true;  
-                //add into safe sequence
-                safeSeq.push_back(i);
+
+    int count=0;
+    while(count < n) {  
+        for(int i=0;i<n;i++) {
+            if(finish[i]==false) {
+                int j;
+                for(j=0;j<m;j++) {
+                    if(need[i][j] > work[j])
+                        break;
+                }
+                // need[i][j] <= work[j]
+                if(j == m)
+                {
+                    for(int k=0;i<m;k++) 
+                        work[k] += allocation[i][k];
+                    finish[i] = true;  
+                    //add into safe sequence
+                    safeSeq[count++] = i;
+                }
+                // cout<<"\n";
             }
-            cout<<"\n";
         }
     }
+
     for(int i=0;i<n;i++) {
         if(finish[i] == false) {
             cout<<"\nNo safe sequence\n";
@@ -66,7 +81,10 @@ vector<int> safety(int max_resources[n][m], int available[m], int need[n][m], in
         }
 
     }
-    return safeSeq;
+
+    for(int i=0;i<n;i++)
+        cout<<safeSeq[i]<<" ";
+    // return safeSeq;
 }
 
 int main() {
@@ -80,10 +98,10 @@ int main() {
     // cout<<"Enter the number of processes";
     // cin>>n;
     int max_resources[n][m] = {{7, 5, 3},
-                     {3, 2, 2},
-                     {9, 0, 2},
-                     {2, 2, 2},
-                     {4, 3, 3}};
+                                {3, 2, 2},
+                                {9, 0, 2},
+                                {2, 2, 2},
+                                {4, 3, 3}};
     // int max_resources[n][m];
     // for(int i=0;i<n;i++) {
     //     cout<<"\nEnter maximum resources requirement for process "<<i+1<<" : ";
@@ -106,17 +124,17 @@ int main() {
     //         cin>>max_resources[i][j];
     // }
 
-    int need[n][m];
-    calculateNeed(need, max_resources, allocation);
-    for(int i=0;i<n;i++) {
-        for(int j=0;j<m;j++)
-            cout<<need[i][j]<<" ";
-        cout<<"\n";
-    }
 
-    vector<int> safe = safety(max_resources,available,need, allocation);
-    for(int i=0;i<n;i++)
-        cout<<safe[i]<<" ";
-    cout<<"\n";
+    // for(int i=0;i<n;i++) {
+    //     for(int j=0;j<m;j++)
+    //         cout<<need[i][j]<<" ";
+    //     cout<<"\n";
+    // }
+
+    cout<<"Calling the function";
+    safety(max_resources,available, allocation);
+    // for(int i=0;i<n;i++)
+    //     cout<<safe[i]<<" ";
+    // cout<<"\n";
     return 0;
 }
