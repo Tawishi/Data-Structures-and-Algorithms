b3de881be636ccc651493ec86f28bca00b9ff467
Tawishi <tawishisharma1@gmail.com>
2021-07-09 22:20:26 +0530

    File paths changed

--- OS_practical/transpose_matrix.cpp
+++ OS_practical/transpose_matrix.cpp
@@ -1,31 +1,30 @@
 #include<iostream> 
 using namespace std; 
 int r,c; 
-void readMatrix(int *a) { //function for reading matrix from user 
-for(int i=0;i<r;i++) 
-for(int j=0;j<c;j++) 
-cin>>*(a + i*r + j); 
-} 
-void showMatrix(int *a, int r, int c) { //function for showing matrix in output 
-for(int i=0;i<r;i++) { 
-for(int j=0;j<c;j++) 
-cout<<*(a + i*r + j)<<" "; 
-cout<<endl; 
-} 
-} 
 int main() { 
-cout<<"Enter number of rows and columns : "; 
-cin>>r>>c; 
-int a[r][c], t[c][r]; 
-cout<<"\nEnter matrix : "; 
-readMatrix(a[0]);
-cout<<"\nMatrix :\n";
-showMatrix(a[0],r,c);
-cout<<"\nTranspose of matrix is : \n"; 
-for(int i=0;i<r;i++) 
-for(int j=0;j<c;j++) 
-t[i][j] = a[j][i]; //transpose of matrix
-showMatrix(t[0],c,r);
-return 0; 
-} 
+    cout<<"Enter number of rows and columns : "; 
+    cin>>r>>c; 
+    int a[r][c], t[c][r]; 
+    cout<<"\nEnter matrix : "; 
+    for(int i=0;i<r;i++) 
+        for(int j=0;j<c;j++) 
+            cin>>a[i][j]; 
+    cout<<"\nMatrix :\n";
+    for(int i=0;i<r;i++) {
+        cout<<"\n"; 
+        for(int j=0;j<c;j++) 
+            cout<<a[i][j]<<" ";
+    }  
+    cout<<"\nTranspose of matrix is : \n"; 
+    for(int i=0;i<r;i++) 
+        for(int j=0;j<c;j++) {
+            t[j][i] = a[i][j]; //transpose of matrix
+        }
+    for(int i=0;i<c;i++) {
+        cout<<"\n";
+        for(int j=0;j<r;j++) 
+            cout<<t[i][j]<<" ";
+    }
+    return 0;
+}
 
